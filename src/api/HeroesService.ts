4db4ba7db5a02d6b3a0b0d849b5aaac572edee45
hacksJuanDavid <josuacoty000@gmail.com>
2024-03-07 11:56:16 -0500

    Update method search by name

--- src/api/HeroesService.ts
+++ src/api/HeroesService.ts
@@ -4,22 +4,42 @@ import { Hero } from "../interfaces/HeroInterface";
 
 // Class to manage the heroes
 export class HeroesService {
+    // Data with the heroes
+    private dataHero = DataHeroesJSON;
+
+
+    // Constructor
+    constructor() {
+        this.dataHero = DataHeroesJSON;
+    }
+
     // Method to get all the heroes
     public getHeroes(): Hero[] {
-        return DataHeroesJSON;
+        return this.dataHero;
     }
 
     // Method to get a hero by id
     public getHeroById(id: number): Hero {
-        return DataHeroesJSON.find(hero => hero.id === id) as Hero;
+        return this.dataHero.find(hero => hero.id === id) as Hero;
     }
 
     // Method to get a hero by name
     public getHeroBySearchName(searchName: string): Hero[] {
-        const findHero = DataHeroesJSON.filter(hero => hero.name.toLowerCase().includes(searchName.toLowerCase()));
-        
-        // Parse the result
-        return findHero;
+        // Normalize the search name
+        const searchNameNormalized = searchName.trim().toLowerCase().replace(/[\s\-_]+/g, '');
+
+        // Filter the heroes
+        const foundHero = this.dataHero.filter(hero => {
+            // Normalize the hero name
+            const heroNameNormalized = hero.name.trim().toLowerCase().replace(/[\s\-_]+/g, '');
+
+            // Return the hero if the name contains the search name
+            return heroNameNormalized.includes(searchNameNormalized);
+        }
+        );
+
+        // Return the hero
+        return foundHero;
     }
 }
 
